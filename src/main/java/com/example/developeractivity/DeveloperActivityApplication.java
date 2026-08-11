91a94a9978ad5922c4e9751cd13a77d849e60cbb
PG Kim <vakon1@daum.net>
2026-08-11 22:58:53 +0900

    chore: initialize Spring Boot 4 project

---
+++ src/main/java/com/example/developeractivity/DeveloperActivityApplication.java
@@ -0,0 +1,13 @@
+package com.example.developeractivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperActivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/developeractivity/DeveloperActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.developeractivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
